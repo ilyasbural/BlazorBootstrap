32d17810d7eeb6312d3e034c3119427021ccd1e5
ilyas bural <ilyasbural@gmail.com>
2025-04-02 01:48:22 +0300

    notika bootstrap template installed

--- BlazorBootstrap.Notika/Program.cs
+++ BlazorBootstrap.Notika/Program.cs
@@ -2,10 +2,8 @@ using BlazorBootstrap.Notika;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-await builder.Build().RunAsync();
+WebAssemblyHostBuilder Builder = WebAssemblyHostBuilder.CreateDefault(args);
+Builder.RootComponents.Add<App>("#app");
+Builder.RootComponents.Add<HeadOutlet>("head::after");
+Builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(Builder.HostEnvironment.BaseAddress) });
+await Builder.Build().RunAsync();
